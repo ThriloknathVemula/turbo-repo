a9ba12ec036f25e70ca648bfb8dccdb593ed79fc
ThriloknathVemula <thriloknath15vemula@gmail.com>
2024-07-28 18:37:44 +0530

    Mono repo using Turborepo

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import { Admin } from "@repo/ui/admin";
+import {InputBox} from "@repo/ui/input-box"
+
+export default function AdminPage(){
+    return <div>
+        Hi from Admin Page
+        <Admin/>
+        <InputBox/>
+    </div>
+}
