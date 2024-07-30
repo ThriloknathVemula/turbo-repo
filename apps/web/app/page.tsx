3c37eaf78213ac31cda6e49de65609aed5adf58f
ThriloknathVemula <thriloknath15vemula@gmail.com>
2024-07-30 23:50:24 +0530

    turborepo practice

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,16 @@
+import { Admin } from "@repo/ui/admin";
 import { Button } from "@repo/ui/button";
+import { InputBox } from "@repo/ui/input-box";
 
 
 export default function Home() {
   return (
-    <Button appName="Web App">
-      Hi there
-    </Button>
+    <>
+      <Button appName="Web App">
+        Hi there
+      </Button>
+      <Admin/>
+      <InputBox/>
+    </>
   );
 }
