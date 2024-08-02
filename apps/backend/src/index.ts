9b5de8fd47d588d14f03043e2c48316e1707e53f
ThriloknathVemula <thriloknath15vemula@gmail.com>
2024-08-02 13:32:02 +0530

    added common module

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import {BACKEND_URL} from '@repo/common/config'
 
 const app = express();
 
@@ -6,6 +7,9 @@ app.get('/',(req,res)=>{
     res.json({message:"Hello World!"});
 })
 
+app.get('/backend',(req,res)=>{
+    res.json({message:`${BACKEND_URL}`});
+})
 
 app.listen(3000,()=>{
     console.log("Server listening on port 3000");
---
+++ packages/common/src/index.ts
@@ -0,0 +1,2 @@
+export const BACKEND_URL = 'Backend url used across other modules'
+export const VALUE = 'random value'
